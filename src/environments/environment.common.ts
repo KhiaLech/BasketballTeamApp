160912daaa3fc888fd1c71147740437c0de86e1a
KhiaLech <48078072+KhiaLech@users.noreply.github.com>
2020-12-03 11:17:22 +1100

    complete

---
+++ src/environments/environment.common.ts
@@ -0,0 +1,10 @@
+export const environment = {
+        firebaseConfig: {
+        apiKey: "AIzaSyDvXf3LNzTgYdzLm4dNcwhCUR8vCTJVtWk",
+        authDomain: "basketballteamapp.firebaseapp.com",
+        projectId: "basketballteamapp",
+        storageBucket: "basketballteamapp.appspot.com",
+        messagingSenderId: "1028536421619",
+        appId: "1:1028536421619:web:dccaff3d701b60e375fb86"
+  },
+};
